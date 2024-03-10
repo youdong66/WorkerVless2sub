ff82901557c8a98cbadf152c460fdd4ed3b9ddaf
youdong66 <141720103+youdong66@users.noreply.github.com>
2024-03-10 22:35:39 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -13,7 +13,7 @@ let addressesapi = [
 
 let DLS = 4;//速度下限
 let addressescsv = [
-	//'https://raw.githubusercontent.com/cmliu/WorkerVless2sub/main/addressescsv.csv' //iptest测速结果文件。
+	'https://github.com/youdong66/WorkerVless2sub/blob/main/addressescsv.csv'
 ];
 
 let subconverter = "api.v1.mk"; //在线订阅转换后端，目前使用肥羊的订阅转换功能。支持自建psub 可自行搭建https://github.com/bulianglin/psub
